0ff1f2f995ef4eab9a4ee7124a6f79640deb0dad
TheJN <julienigou@outlook.com>
2022-10-26 10:15:17 +0200

    V1 APP

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
-import streamlit as st 
+import streamlit as st
 import pandas as pd
+import numpy as np
 
-st.write("Projet Aerodynamique")
+
+st.set_page_config(
+    page_title="Hypersonique",
+    page_icon="👋",
+)
+
+st.header("Outil de prévision rapide des propriétés aérodynamiques en régime hypersonique ")
+
+st.sidebar.title("Configuration")
+st.sidebar.multiselect('Geométrie', ['Nez pointu', 'sphère'])
+
+st.sidebar.slider('Nombre de Mach', 5, 8)
+st.sidebar.slider('Température', 0, 100)
+st.sidebar.slider('Pression', 0, 100)
+
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['a', 'b', 'c'])
+
+st.write("Reapartion de la température")
+
+st.line_chart(chart_data)
